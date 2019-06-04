7c89a738db85e258864b062d4148d1e5ffb5c248
Scala Steward <me@scala-steward.org>
2019-06-04 18:27:35 +0200

    Update semanticdb-scalac to 4.1.11

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -32,7 +32,7 @@ object ProjectPlugin extends AutoPlugin {
       autoAPIMappings in Global := true,
       addCompilerPlugin(Dependencies.CompilerPlugin.kindProjector),
       addCompilerPlugin(Dependencies.CompilerPlugin.monadicFor),
-      addCompilerPlugin(scalafixSemanticdb("4.1.10")),
+      addCompilerPlugin(scalafixSemanticdb("4.1.11")),
       scalacOptions ++= commonScalacOptions ++ scalacOptionsFor212 ++ semanticdbOptions
     )
 
