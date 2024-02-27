eef105d71b5a772bb3d23b608059fe7fd4a0b974
Scala Steward <scala_steward@virtuslab.com>
2024-02-27 18:42:26 +0000

    Update scala-library to 2.13.13

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -19,7 +19,7 @@ object ProjectPlugin extends AutoPlugin {
   override val buildSettings: Seq[Def.Setting[_]] = Seq(
     scalafixDependencies += Dependencies.scaluzzi,
     organization := "enzief",
-    scalaVersion := "2.13.4",
+    scalaVersion := "2.13.13",
   )
 
   override val projectSettings: Seq[Def.Setting[_]] =
